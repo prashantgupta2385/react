66134b4147623f81a3f3689a405add60d7047cfd
prashantgupta2385 <prashantgupta20august@gmail.com>
2024-11-08 23:26:29 +0530

    basic file structure

---
+++ 01basicreact/src/chai.js
@@ -0,0 +1,6 @@
+function Chai(){
+    return (
+        <h1>chai by prashant </h1>
+    )
+}
+export default Chai;
---
+++ 01vitebasic/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from './chai'
+function App() {
+  
+
+  return (
+    <>
+    <h1>hello world</h1>
+    <Chai/>
+   </>
+  )
+}
+
+export default App
